99da4b9290251c2bcc98a08dc9ffb8ae18a2cfe0
xinghx <herry_xing@126.com>
2018-12-09 18:04:45 +0800

    add imagelib

---
+++ imageloader/src/main/java/com/youweihui/imageloader/ImageLoader.java
@@ -0,0 +1,18 @@
+package com.youweihui.imageloader;
+
+import android.content.Context;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+
+public final class ImageLoader {
+    private static final String TAG = "ImageLoader";
+
+    private ImageLoader() {
+
+    }
+
+    public static void showToImageView(Context mContext, ImageView imageView, String target) {
+        Glide.with(mContext).load(target).into(imageView);
+    }
+}
